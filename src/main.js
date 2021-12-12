0117f86a3f38c028e4c23a638f2d43dbdbf2c0cf
lzs6666 <1057319877@qq.com>
2021-12-13 01:48:32 +0800

    qiankun-base
    base

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import { registerMicroApps, start } from 'qiankun';
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
+
+registerMicroApps([
+
+  {
+    name: 'vueApp',
+    entry: '//localhost:8888',
+    container: '#vueApp',
+    activeRule: '/vue',
+  },
+
+]);
+// 启动 qiankun
+start();
